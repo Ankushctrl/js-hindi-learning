ea2383a9b40775003d5b95f3d3cea8c274d08c90
ankushbanotra <ankushbanotra122@gmail.com>
2026-07-11 17:31:29 +0530

    study abhout variabls  and clear all concetps

---
+++ 01_basics/1_var.js
@@ -0,0 +1,6 @@
+const accountId = 2302031000001
+let  accountEmail =  "2302031000001@gmail.com";
+var accountPassword = "1234456"
+accountCity = "kathua"
+
+console.table(  [accountEmail,accountId,accountPassword, accountCity]);
