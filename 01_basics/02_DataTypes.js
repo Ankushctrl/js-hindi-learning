4e670ee705fdad411782633e28e28b3fe00792d6
ankushbanotra <ankushbanotra122@gmail.com>
2026-07-12 16:58:25 +0530

    conversion and operations

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"// treat all js code as newer version 
+
+// alart( 3 + 3) { we are using node js , not brower and alart is used for to send a popup to user }
+//There are the some Important Data Types 
+/* 
+String 
+Number and BigInt ,  Number$2^{53} - 1 max limit  , when increase the limit of digitals so we can use "BigInt" Data Types becasue of it is infinity unlimited digit are used 
+
+Boolean
+undefined
+Null  standalone value 
+symbol 
+and objects 
+*/
+
+let  age  = 18
+let  Name ="ankush"  
+let  isLoggedIn  = true
+ 
+
+console.table([ age,Name, isLoggedIn,])
+console.log(typeof NaN);
+console.log(typeof Infinity);
+
+// type of undefined is undefinednad 
+// type of null is object 
+// type of string is function 
