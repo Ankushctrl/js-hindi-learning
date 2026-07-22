190235bc82e0ac3cee31c108750b7cd9ca8c0f5d
ankushbanotra <ankushbanotra122@gmail.com>
2026-07-22 19:14:10 +0530

    leran new prototyes in numbers

--- 01_basics/Number_And_Math.js
+++ 01_basics/Number_And_Math.js
@@ -4,6 +4,14 @@
 
  const balance = new Number(100)
 
- console.log(balance);
- console.log(typeof balance);
+//  console.log(balance);
+ console.log(balance.toString().length);
+ console.log(balance.toFixed(3));
+//  console.log(typeof score  );
+
+const otherNumber = 126.15244
+console.log(otherNumber.toPrecision(4));
+
+ const hunderds = 100000000000;
+ console.log(hunderds.toLocaleString());
  
